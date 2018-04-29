4df1436dea214e2e6fe97004cc5a047c4b49f0d3
Chase Moskal <chasemoskal@gmail.com>
2018-04-29 00:20:56 -0700

    eliminate main script in favor of global bundle

--- build.js
+++ build.js
@@ -28,8 +28,8 @@ const buildOptions = {
 	sassWatch: commander.sassWatch,
 	paths: {
 		nb: "$(npm bin)/",
-		scriptSource: "source/shopperman.global.tsx",
-		scriptBundle: "dist/shopperman.global.bundle.js",
+		scriptSource: "source/shopperman.tsx",
+		scriptBundle: "dist/shopperman.bundle.js",
 		styleSource: "source/shopperman.scss",
 		styleOutput: "dist/shopperman.css",
 		polyfills: [
