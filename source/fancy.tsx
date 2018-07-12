651eb144daca6e5f4a2faf215c9b90ff580c511c
Chase Moskal <chasemoskal@gmail.com>
2018-07-12 16:34:50 -0700

    add fancy demo function

---
+++ source/fancy.tsx
@@ -0,0 +1,87 @@
+
+import {h} from "preact"
+import * as crnc from "crnc"
+import * as mobx from "mobx"
+import * as shopperman from "."
+import * as preact from "preact"
+import { CurrencyControlOptions } from "./stores"
+import { ShopifySettings } from "./shopify"
+
+export interface Shopperman1Options {
+	currency: CurrencyControlOptions
+	shopify: ShopifySettings
+	collectionId: string
+	cartSystem: {
+		checkoutInNewWindow: boolean
+	}
+	renderTargets: {
+		productsArea: HTMLElement
+		cartArea: HTMLElement
+	}
+}
+
+//
+// SHOPPERMAN DEMO FUNCTION
+//
+
+export async function shopperman1(options: Shopperman1Options) {
+
+	//
+	// basic settings
+	//
+
+	mobx.configure({enforceActions: true})
+
+	//
+	// instances
+	//
+
+	const currencyControl = new shopperman.CurrencyControl(options.currency)
+	const shopifyAdapter = new shopperman.ShopifyAdapter({
+		settings: options.shopify,
+		currencyControl
+	})
+
+	//
+	// fetch products from shopify
+	//
+
+	const products = await shopifyAdapter.getProductsInCollection(options.collectionId)
+
+	//
+	// create cart model
+	//
+
+	const cart = new shopperman.Cart({
+		currencyControl,
+		itemCatalog: products.map(product =>
+			new shopperman.CartItem({
+				product,
+				currencyControl,
+				quantityMin: 1,
+				quantityMax: 5
+			}))
+	})
+
+	//
+	// render components
+	//
+
+	preact.render(
+		<div className="product-list">
+			{products.map(product =>
+				<shopperman.ProductDisplay {...{cart, product}}/>
+			)}
+		</div>,
+		options.renderTargets.productsArea
+	)
+
+	preact.render(
+		<shopperman.CartSystem {...{
+			cart,
+			checkoutMachine: shopifyAdapter.checkoutMachine,
+			checkoutInNewWindow: options.cartSystem.checkoutInNewWindow
+		}}/>,
+		options.renderTargets.cartArea
+	)
+}
