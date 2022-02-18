c2a894075de00d077d67a9d93daee7b3c6b9eb87
Chase Moskal <chasemoskal@gmail.com>
2022-02-18 11:58:18 -0800

    improve: customizability of loaders

--- s/framework/loadable-component.ts
+++ s/framework/loadable-component.ts
@@ -1,13 +1,16 @@
 
 import {property} from "lit/decorators.js"
-import {html, css, svg, CSSResultArray} from "lit"
+import {html, css, CSSResultArray} from "lit"
 
 import {ShopperComponent} from "./shopper-component.js"
+
+import loaderSvg from "../icons/feather/loader.svg.js"
+import alertTriangleSvg from "../icons/feather/alert-triangle.svg.js"
 
 export enum LoadableState {
 	Loading,
 	Error,
-	Ready
+	Ready,
 }
 
 const _state = Symbol()
@@ -22,13 +25,19 @@ export class LoadableComponent extends ShopperComponent {
 	set loadableState(value: LoadableState) { this[_state] = value }
 	get loadableState(): LoadableState { return this[_state] }
 
+	@property()
+	loadingIcon = loaderSvg
+
+	@property()
+	errorIcon = alertTriangleSvg
+
 	static get styles(): CSSResultArray {return [css`
 		.loadable {
 			display: flex;
 			align-items: center;
 			justify-content: center;
 			font-family: monospace;
-			color: #444;
+			color: var(--shopper-loading-color, currentColor);
 		}
 
 		.loadable svg {
@@ -55,7 +64,7 @@ export class LoadableComponent extends ShopperComponent {
 		}
 
 		.loadable.error {
-			color: maroon;
+			color: var(--shopper-loading-error, maroon);
 		}
 	`]}
 
@@ -74,7 +83,7 @@ export class LoadableComponent extends ShopperComponent {
 	private [_renderLoading]() {
 		return html`
 			<div class="loadable loading">
-				${svg`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader"><line x1="12" y1="2" x2="12" y2="6"></line><line x1="12" y1="18" x2="12" y2="22"></line><line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line><line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line><line x1="2" y1="12" x2="6" y2="12"></line><line x1="18" y1="12" x2="22" y2="12"></line><line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line><line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line></svg>`}
+				${this.loadingIcon}
 				<p>${this.loadingMessage}</p>
 			</div>
 		`
@@ -83,7 +92,7 @@ export class LoadableComponent extends ShopperComponent {
 	private [_renderError]() {
 		return html`
 			<div class="loadable error">
-				${svg`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-triangle"><path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path><line x1="12" y1="9" x2="12" y2="13"></line><line x1="12" y1="17" x2="12" y2="17"></line></svg>`}
+				${this.errorIcon}
 				<p>${this.errorMessage}</p>
 			</div>
 		`
